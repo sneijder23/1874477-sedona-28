c99f3d2592bf18269666e8e2bba215092031e8cd
Alexander <sneijder23@ya.ru>
2023-04-27 19:30:05 +0300

    Адаптивная графика

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+// Мобильное меню сайта
 const mainNavToggle = document.querySelector('.main-nav__toggle');
 const mainNavSiteList = document.querySelector('.main-nav');
 
@@ -10,4 +11,34 @@ mainNavToggle.addEventListener('click', function () {
   } else {
     mainNavSiteList.classList.replace('main-nav--open', 'main-nav--close');
   }
+});
+
+
+// Модальное окно страницы Форма
+const form = document.querySelector('.form');
+const errorModal = document.querySelector('.modal__container--error');
+const successModal = document.querySelector('.modal__container--success');
+const successCloseButton = successModal.querySelector('.modal__button');
+const errorCloseButton = errorModal.querySelector('.modal__button');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); // предотвращаем отправку формы по умолчанию
+
+  // проверяем, прошла ли валидация формы
+  if (form.checkValidity()) {
+    // если успешно
+    successModal.classList.add('modal__container--open');
+  } else {
+    // если есть ошибки
+    errorModal.classList.add('modal__container--open');
+  }
+});
+
+
+successCloseButton.addEventListener('click', function () {
+  successModal.classList.remove('modal__container--open');
+});
+
+errorCloseButton.addEventListener('click', function () {
+  errorModal.classList.remove('modal__container--open');
 });
