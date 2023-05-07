e700c9794f61122f211e6dbae43c38d156e85205
Alexander <sneijder23@ya.ru>
2023-05-07 12:06:43 +0300

    Open/close modal

--- source/js/script.js
+++ source/js/script.js
@@ -19,3 +19,48 @@ if (window.location.pathname === '/index.html') {
 
   mapImage.classList.remove('map__wrapper--nojs')
 }
+
+
+const formElement = document.querySelector('.form__submit');
+
+formElement?.addEventListener('click', (event) => {
+  event.preventDefault();
+  const name = document.querySelector('#name');
+  const surname = document.querySelector('#surname');
+  const tel = document.querySelector('#telephone');
+  const email = document.querySelector('#email');
+
+
+  const isNameError = name.value === '';
+  const isSurnameError = surname.value === '';
+  const isTelephoneError = tel.value === '' || !tel.value.match(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/);
+  const isEmailError = email.value === '' || !email.value.match(/^[\w.-]+@[\w.-]+\.[A-Za-z]{2,4}$/);
+
+  if (isNameError || isSurnameError || isTelephoneError || isEmailError) {
+    document.querySelector('.modal__container--error').classList.add('modal__container--open');
+  } else {
+    document.querySelector('.modal__container--success').classList.add('modal__container--open');
+  }
+});
+
+if (window.location.pathname === '/form.html') {
+  const modalContainers = document.querySelectorAll('.modal__container');
+  const closeButtonError = document.querySelector('.modal__button--main');
+  const closeButtonSuccess = document.querySelector('.modal__button--secondary');
+
+  closeButtonError?.addEventListener('click', () => {
+    modalContainers.forEach((container) => {
+      if (container.classList.contains('modal__container--error')) {
+        container.classList.remove('modal__container--open');
+      }
+    })
+  });
+
+  closeButtonSuccess?.addEventListener('click', () => {
+    modalContainers.forEach((container) => {
+      if (container.classList.contains('modal__container--success')) {
+        container.classList.remove('modal__container--open');
+      }
+    })
+  });
+};
